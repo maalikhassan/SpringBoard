6af8c5a9919166fede488103eb7498cf9882da72
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-29 17:13:30 +0530

    Refactor:delete the Cofig file

--- src/main/java/com/springboard/config/Config.java
+++ src/main/java/com/springboard/config/Config.java
@@ -3,6 +3,7 @@ package com.springboard.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
